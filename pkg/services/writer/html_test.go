14cd9099bfddea5e74c222400153b75d86f9a81a
Denys G. Santos <gsdenys@gmail.com>
2022-12-07 15:25:03 -0500

    add unit test for html output

--- pkg/services/writer/html_test.go
+++ pkg/services/writer/html_test.go
@@ -6,8 +6,26 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/gsdenys/pdgen/pkg/models"
+	"github.com/gsdenys/pdgen/pkg/services/translate"
 	"github.com/stretchr/testify/assert"
 )
+
+const wantColumns string = "\t<table class=\"styled-table\">\n\t\t<tr class=\"active-row\">\n\t\t\t<th>table-title-name</th>\n\t\t\t<th>table-title-type</th>\n\t\t\t<th>table-title-allow</th>\n\t\t\t<th>table-title-comment</th>\n\t\t</tr>\n\t\t<tr>\n\t\t\t<td>name</td>\n\t\t\t<td>text</td>\n\t\t\t<td></td>\n\t\t\t<td>Somme comment</td>\n\t\t</tr>\n\t\t<tr>\n\t\t\t<td>type</td>\n\t\t\t<td>text</td>\n\t\t\t<td>BASE EXTENSION</td>\n\t\t\t<td>Another comment</td>\n\t\t</tr>\n\t</table>\n"
+
+var columns []models.Columns = []models.Columns{
+	{
+		Column:  "name",
+		Type:    "text",
+		Allow:   "",
+		Comment: "Somme comment",
+	},
+	{
+		Column:  "type",
+		Type:    "text",
+		Allow:   "BASE EXTENSION",
+		Comment: "Another comment",
+	},
+}
 
 func TestPrinterHTML_Init(t *testing.T) {
 	file := getWorkDir() + uuid.NewString()
@@ -111,20 +129,75 @@ func TestPrinterHTML_Body(t *testing.T) {
 	assert.Equal(t, string(f), "\t<p>test</p>\n")
 }
 
-// func TestPrinterHTML_Column(t *testing.T) {
-// 	file := getWorkDir() + uuid.NewString()
+func TestHTML_Columns(t *testing.T) {
+	file := getWorkDir() + uuid.NewString()
 
-// 	p := &PrinterHTML{
-// 		Out:       createFile(file),
-// 		Translate: translate.GetTranslation("en"),
-// 	}
+	html := &HTML{
+		Out: createFile(file),
+	}
 
-// 	p.Body("test")
+	translate.InitLanguage()
+	html.Columns(columns)
 
-// 	f, err := os.ReadFile(file)
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
+	f, err := os.ReadFile(file)
+	if err != nil {
+		t.Error(err)
+	}
 
-// 	assert.Equal(t, string(f), "\t<p>TEST</p>\n")
-// }
+	assert.Equal(t, string(f), wantColumns)
+}
+
+func TestHTML_SetWriter(t *testing.T) {
+	file := getWorkDir() + uuid.NewString()
+
+	p := &HTML{}
+	assert.Nil(t, p.Out)
+
+	p.SetWriter(file)
+	assert.NotNil(t, p.Out)
+}
+
+func TestHTML_Table(t *testing.T) {
+	file := getWorkDir() + uuid.NewString()
+
+	html := &HTML{
+		Out: createFile(file),
+	}
+
+	translate.InitLanguage()
+
+	table := models.Table{
+		Name:    "test",
+		Desc:    "Table test",
+		Columns: columns,
+	}
+
+	html.Table(table)
+
+	f, err := os.ReadFile(file)
+	if err != nil {
+		t.Error(err)
+	}
+
+	want := "\t<h3>TEST</h3>\n\t<p>Table test</p>\n\t<br>\n" + wantColumns + "\t<br>\n"
+
+	assert.Equal(t, string(f), want)
+}
+
+func TestHTML_Done(t *testing.T) {
+	file := getWorkDir() + uuid.NewString()
+
+	html := &HTML{
+		Out: createFile(file),
+	}
+
+	translate.InitLanguage()
+	html.Done(models.Describe{})
+
+	f, err := os.ReadFile(file)
+	if err != nil {
+		t.Error(err)
+	}
+
+	assert.Equal(t, string(f), "\n</body>\n\n</html>")
+}
